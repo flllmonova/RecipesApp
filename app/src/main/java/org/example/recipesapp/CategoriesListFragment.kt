8a73148172ad69721246146e734b3849f3cbdd3b
flllmonova <flllmonova@yandex.ru>
2025-09-06 04:43:20 +0500

    RA-10: 1. Transfer data to the recipe screen via Bundle; 2. Update images, change their format from png to webp; 3. Write the layout of the recipes list header, fill in with data from the bundle.

--- app/src/main/java/org/example/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/org/example/recipesapp/CategoriesListFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import org.example.recipesapp.databinding.FragmentListCategoriesBinding
+
+const val ARG_CATEGORY_ID = "categoryId"
+const val ARG_CATEGORY_NAME = "categoryName"
+const val ARG_CATEGORY_IMAGE_URL = "categoryImageUrl"
 
 class CategoriesListFragment : Fragment() {
     private var _binding: FragmentListCategoriesBinding? = null
@@ -27,8 +32,8 @@ class CategoriesListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        initRecycler()
         super.onViewCreated(view, savedInstanceState)
+        initRecycler()
     }
 
     override fun onDestroyView() {
@@ -40,15 +45,23 @@ class CategoriesListFragment : Fragment() {
         val adapter = CategoriesListAdapter(STUB.getCategories())
         binding.rvCategories.adapter = adapter
         adapter.setOnItemClickListener(object : CategoriesListAdapter.OnItemClickListener {
-            override fun onItemClick() {
-                openRecipesByCategoryId()
+            override fun onItemClick(categoryId: Int) {
+                openRecipesByCategoryId(categoryId)
             }
         })
     }
 
-    private fun openRecipesByCategoryId() {
+    private fun openRecipesByCategoryId(categoryId: Int) {
+        val categoryName = STUB.getCategories()[categoryId].title
+        val categoryImageUrl = STUB.getCategories()[categoryId].imageUrl
+        val bundle: Bundle = bundleOf(
+            ARG_CATEGORY_ID to categoryId,
+            ARG_CATEGORY_NAME to categoryName,
+            ARG_CATEGORY_IMAGE_URL to categoryImageUrl
+        )
+
         activity?.supportFragmentManager?.commit {
-            replace<RecipesListFragment>(R.id.mainContainer)
+            add<RecipesListFragment>(R.id.mainContainer, args = bundle)
             setReorderingAllowed(true)
             addToBackStack("Recipes List Fragment")
         }
--- app/src/main/java/org/example/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/org/example/recipesapp/RecipesListFragment.kt
@@ -1,11 +1,14 @@
 package org.example.recipesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.example.recipesapp.databinding.FragmentRecipesListBinding
+import java.io.InputStream
 
 class RecipesListFragment : Fragment() {
 
@@ -15,6 +18,10 @@ class RecipesListFragment : Fragment() {
             "Binding for FragmentRecipesListBinding must not be null"
         )
 
+    var categoryId: Int? = null
+    var categoryName: String? = null
+    var categoryImageUrl: String? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +30,22 @@ class RecipesListFragment : Fragment() {
             inflater, container, false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryId = requireArguments().getInt(ARG_CATEGORY_ID)
+        categoryName = requireArguments().getString(ARG_CATEGORY_NAME)
+        categoryImageUrl = requireArguments().getString(ARG_CATEGORY_IMAGE_URL)
+        binding.categoryName.text = categoryName
+        try {
+            val inputStream: InputStream? =
+                binding.root.context?.assets?.open(categoryImageUrl.toString())
+            val drawable = Drawable.createFromStream(inputStream, null)
+            binding.categoryImage.setImageDrawable(drawable)
+        } catch (e: Exception) {
+            Log.e("Image not found", Log.getStackTraceString(e))
+        }
     }
 
     override fun onDestroyView() {
