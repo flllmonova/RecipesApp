28a8f678fe1cfd8e2bafe69fabfb8855d9a0d939
flllmonova <flllmonova@yandex.ru>
2025-09-09 02:44:42 +0500

    RA-13 implement screen Recipe

--- app/src/main/java/org/example/recipesapp/RecipeFragment.kt
+++ app/src/main/java/org/example/recipesapp/RecipeFragment.kt
@@ -1,12 +1,17 @@
 package org.example.recipesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import org.example.recipesapp.databinding.FragmentRecipeBinding
+import java.io.InputStream
 
 class RecipeFragment : Fragment() {
 
@@ -15,6 +20,7 @@ class RecipeFragment : Fragment() {
         get() = _binding ?: throw IllegalStateException(
             "Binding for FragmentRecipeBinding must not be null"
         )
+    private var recipe: Recipe? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,16 +34,60 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
         } else {
             arguments?.getParcelable(ARG_RECIPE)
         }
-        binding.tvRecipeTitle.text = recipe?.title
+        initUI()
+        initRecycler()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initUI() {
+        binding.tvRecipeTitle.text = recipe?.title ?: ""
+        try {
+            val inputStream: InputStream? =
+                binding.root.context?.assets?.open(recipe?.imageUrl ?: "")
+            val drawable = Drawable.createFromStream(inputStream, null)
+            binding.ivRecipe.setImageDrawable(drawable)
+        } catch (e: Exception) {
+            Log.e("Image not found", Log.getStackTraceString(e))
+        }
+    }
+
+    private fun initRecycler() {
+        initIngredientsRecycler()
+        initMethodRecycler()
+    }
+
+    private fun initIngredientsRecycler() {
+        val adapter = IngredientsAdapter(recipe?.ingredients ?: listOf())
+        binding.rvIngredients.adapter = adapter
+        binding.rvIngredients.addItemDecoration(initDivider())
+    }
+
+    private fun initMethodRecycler() {
+        val adapter = MethodAdapter(recipe?.method ?: listOf())
+        binding.rvMethod.adapter = adapter
+        binding.rvMethod.addItemDecoration(initDivider())
+
+    }
+
+    private fun initDivider(): MaterialDividerItemDecoration {
+        val context = binding.root.context
+        val divider = MaterialDividerItemDecoration(context, LinearLayout.VERTICAL)
+        divider.apply {
+            isLastItemDecorated = false
+            dividerColor = resources.getColor(
+                R.color.navigationBarColor, context.theme
+            )
+            dividerThickness = resources.getInteger(R.integer.dividerThickness)
+        }
+        return divider
     }
 }
