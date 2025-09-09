f44a9a92a647b4ef100e79d247b195990b9b30c2
flllmonova <flllmonova@yandex.ru>
2025-09-09 18:30:28 +0500

    RA-13 delete useless constants variables, update string resource

--- app/src/main/java/org/example/recipesapp/Constants.kt
+++ app/src/main/java/org/example/recipesapp/Constants.kt
@@ -1,7 +1,4 @@
 package org.example.recipesapp
 
-const val ARG_CATEGORY_ID = "categoryId"
-const val ARG_CATEGORY_NAME = "categoryName"
-const val ARG_CATEGORY_IMAGE_URL = "categoryImageUrl"
 const val ARG_CATEGORY = "category"
 const val ARG_RECIPE = "recipe"
